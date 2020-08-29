f82aaeff1895bb3704f43bb3241461d9861cf441
winifredf <winifred.dykes@hotmail.com>
2020-08-29 12:06:00 -0400

    created routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  resources :users
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/login'
+  # get 'sessions/welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+
+  get 'login' to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  delete 'logout', to: 'sessions#destroy'
+
+  root to: 'sessions#welcome'
 end
