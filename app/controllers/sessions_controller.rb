45dedebddcb68b22662f85c23a44859d4088170b
winifredf <winifred.dykes@hotmail.com>
2020-08-30 13:15:36 -0400

    got an invalid redirect error on page. just left off / in front of welcome so I added it!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to 'welcome'
+      redirect_to '/welcome'
     else
       redirect_to 'login'
     end
