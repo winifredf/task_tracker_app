1e0708f0f4879c1451b9dcaf1ec4708a27456168
winifredf <winifred.dykes@hotmail.com>
2020-08-30 13:11:36 -0400

    “for create method I wanted to find the user/friend by username and did params username. if theres a user, then authenticate the user params password. then i set the sessions user id. If everything checks out, redirect user/friend to the welcome page. If not, redirect to login. also deleted methods i will not use just to clean it up a little.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
-
-  def login
-  end
-
-  def welcome
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to 'welcome'
+    else
+      redirect_to 'login'
+    end
   end
 
   def destroy
