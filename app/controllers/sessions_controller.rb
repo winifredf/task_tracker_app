85f1d872f89a6f1a73c88dd78c197e91823a9e55
winifredf <winifred.dykes@hotmail.com>
2020-08-30 13:19:02 -0400

    I'd gotten rid of my login method. Therefore i added it back in my sessions controller :)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def login
+  end
+
+  
+
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
