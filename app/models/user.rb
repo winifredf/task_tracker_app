a13d12a11225715ac01c07a6b4d89b573ff825fa
winifredf <winifred.dykes@hotmail.com>
2020-08-30 18:58:20 -0400

    updated user class in user file with has many user tasks, has may tasks through user tasks, and of couurse has secure password and validates username presence and uniqueness to be true.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    has_many :user_tasks
+    has_many :tasks, through: :user_tasks
+
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
