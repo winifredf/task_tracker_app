e848ec7f3bb5f1aac0463dc89a10a160d23f7c01
winifredf <winifred.dykes@hotmail.com>
2020-08-30 11:46:44 -0400

    added ability to create a new user and log new user in. If successfully logged in, send user/Friend to the welcome page. If user/Friend did not successfully log in, send back to new page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to '/welcome'
+        else
+            render :new
+        end
     end
 
     private
