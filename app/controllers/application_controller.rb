80f4be5b1052f2be31bd03ce6a7b588e60ea387a
winifredf <winifred.dykes@hotmail.com>
2020-08-30 13:33:38 -0400

    redirect to welcome unless the user/friend is logged in. added a before action authorized method to execute before helper methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :authorized
     helper_method :current_user
     helper_method :logged_in?
 
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
 
     def logged_in?
         !current_user.nil?
+    end
+
+    def authorized
+        redirect_to '/welcome' unless logged_in?
     end
 
 end
