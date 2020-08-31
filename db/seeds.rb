cfb399c8a265ccfb9e0bceb53fb32cffc773a5cc
winifredf <winifred.dykes@hotmail.com>
2020-08-30 20:26:11 -0400

    corrected :through: to through: for has_many :users in task.rb. Also I created 110 tasks, gave it a title, and sample true or false.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..110).each do |num|
+    Task.create(title: "title#{num}", complete: [true, false].sample)
+end
