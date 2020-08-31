98ab21646a763b13ff6aa5a92afd9ad535d803dd
winifredf <winifred.dykes@hotmail.com>
2020-08-31 11:14:50 -0400

    wrote a scope method in task.rb and used a bit of syntactic sugar to be able to chain and return my data, while adding more functionality to my project in a much cleaner and simplistic way.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,13 @@ class Task < ApplicationRecord
     has_many :user_tasks
     has_many :users, through: :user_tasks
 
-    def self.completed_tasks
-        self.all.selet{| task | task.complete == true}
-    end
+    scope :completed_tasks, => { where(complete:true)}
+
+    # def self.completed_tasks
+    #     self.all.selet{| task | task.complete == true}
+    # end
+
+    # def self.completed_tasks
+    #     where(complete:true)
+    # end
 end
