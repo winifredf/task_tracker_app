b536f28b9c46757bc2e796116600e5ba25356e93
winifredf <winifred.dykes@hotmail.com>
2020-08-31 15:21:53 -0400

    added ENV APP ID and ENV Secret to omniauth.rb file.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :facebook, ENV['4681391675212147'], ENV['ebd4bf80086e0a525de9b84c114007e1']
   end
