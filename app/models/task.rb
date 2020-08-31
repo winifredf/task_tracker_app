ab5a6318808924296cf65c95f04a49c200a43a2a
winifredf <winifred.dykes@hotmail.com>
2020-08-30 21:07:14 -0400

    updated scope method in task.erb.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
     has_many :user_tasks
     has_many :users, through: :user_tasks
+
+    def self.completed_tasks
+        self.all.selet{| task | task.complete == true}
 end
