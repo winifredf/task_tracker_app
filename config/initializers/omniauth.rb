a968bac3cdad4ba86c66a85a99ebca1fc7267558
winifredf <winifred.dykes@hotmail.com>
2020-08-31 21:25:28 -0400

    changed env in omniauth.rb back to YOUR ID and YOUR SECRET.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['4681391675212147'], ENV['ebd4bf80086e0a525de9b84c114007e1']
+    provider :facebook, ENV['YOUR_ID'], ENV['YOUR_SECRET']
   end
