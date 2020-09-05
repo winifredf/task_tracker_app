d82db1347bb20b88a0837d103a9dc6686c773c7e
winifredf <winifred.dykes@hotmail.com>
2020-09-04 20:49:45 -0400

    changed ENV['YOUR_ID] AND ENV[YOUR_SECRET] TO ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET' ] in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, YOUR_APP_ID, YOUR_APP_SECRET
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   end
